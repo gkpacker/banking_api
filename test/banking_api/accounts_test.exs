8b9803c106d2b075247c67f41a363c1c48708cae
Gabriel <ggkpacker@gmail.com>
2020-08-21 09:41:58 -0300

    Test accounts user validations

--- test/banking_api/accounts_test.exs
+++ test/banking_api/accounts_test.exs
@@ -6,9 +6,11 @@ defmodule BankingApi.AccountsTest do
   describe "users" do
     alias BankingApi.Accounts.User
 
-    @valid_attrs %{email: "some email", encrypted_password: "some encrypted_password"}
-    @update_attrs %{email: "some updated email", encrypted_password: "some updated encrypted_password"}
-    @invalid_attrs %{email: nil, encrypted_password: nil}
+    @valid_attrs %{email: "user@email.com", password: "password"}
+    @update_attrs %{email: "updated@email.com", password: "updated_password"}
+    @invalid_attrs %{email: nil, password: nil}
+    @invalid_email_attrs %{email: 'email', password: "password"}
+    @invalid_password_attrs %{email: 'user@email.com', password: "short"}
 
     def user_fixture(attrs \\ %{}) do
       {:ok, user} =
@@ -16,7 +18,7 @@ defmodule BankingApi.AccountsTest do
         |> Enum.into(@valid_attrs)
         |> Accounts.create_user()
 
-      user
+      Map.put(user, :password, nil)
     end
 
     test "list_users/0 returns all users" do
@@ -25,25 +27,42 @@ defmodule BankingApi.AccountsTest do
     end
 
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
-      assert Accounts.get_user!(user.id) == user
+      created_user = user_fixture()
+      user = Accounts.get_user!(created_user.id)
+
+      assert user == created_user
+      assert user.password == nil
     end
 
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "some email"
-      assert user.encrypted_password == "some encrypted_password"
+      assert user.email == "user@email.com"
+      assert Bcrypt.verify_pass("password", user.encrypted_password)
     end
 
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
 
+    test "create_user/1 with existent email returns error changeset" do
+      user_fixture()
+
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@valid_attrs)
+    end
+
+    test "create_user/1 with invalid email returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_email_attrs)
+    end
+
+    test "create_user/1 with short password returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_password_attrs)
+    end
+
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.email == "some updated email"
-      assert user.encrypted_password == "some updated encrypted_password"
+      assert user.email == "updated@email.com"
+      assert Bcrypt.verify_pass("updated_password", user.encrypted_password)
     end
 
     test "update_user/2 with invalid data returns error changeset" do
