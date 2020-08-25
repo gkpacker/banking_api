c3937656d77d43a9ab1ee104cb34e767301f2602
Gabriel <ggkpacker@gmail.com>
2020-08-25 13:33:04 -0300

    Create accounts model

---
+++ lib/banking_api/bank.ex
@@ -0,0 +1,132 @@
+defmodule BankingApi.Bank do
+  @moduledoc """
+  The Bank context.
+  """
+
+  import Ecto.Query, warn: false
+  alias BankingApi.Repo
+
+  alias BankingApi.Accounts
+  alias BankingApi.Accounts.User
+  alias BankingApi.Bank.Account
+
+  @doc """
+  Returns the list of accounts.
+
+  ## Examples
+
+      iex> list_accounts()
+      [%Account{}, ...]
+
+  """
+  def list_accounts do
+    Repo.all(Account)
+  end
+
+  @doc """
+  Gets a single account.
+
+  Raises `Ecto.NoResultsError` if the Account does not exist.
+
+  ## Examples
+
+      iex> get_account!(123)
+      %Account{}
+
+      iex> get_account!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_account!(id), do: Repo.get!(Account, id)
+
+  @doc """
+  Creates a account.
+
+  ## Examples
+
+      iex> create_account(%{field: value})
+      {:ok, %Account{}}
+
+      iex> create_account(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_account(attrs \\ %{}) do
+    %Account{}
+    |> Account.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Creates user's initial accounts `Cash`, `Withdraws` and `Initial Credit`.
+
+  ## Examples
+
+      iex> create_initial_accounts(%User{})
+      %User{
+        accounts: [
+          %Account{name: "Initial Credit", type: "equity"},
+          %Account{name: "Cash", type: "asset"},
+          %Account{name: "Withdraws", type: "equity", contra: true}
+        ]
+      }
+  """
+  def create_initial_accounts(%User{id: id}) do
+    initial_accounts = [
+      %{name: "Withdraws", type: "equity", contra: true, user_id: id},
+      %{name: "Cash", type: "asset", user_id: id},
+      %{name: "Initial Credit", type: "equity", user_id: id}
+    ]
+
+    for account <- initial_accounts, do: create_account(account)
+
+    Accounts.get_user!(id)
+  end
+
+  @doc """
+  Updates a account.
+
+  ## Examples
+
+      iex> update_account(account, %{field: new_value})
+      {:ok, %Account{}}
+
+      iex> update_account(account, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_account(%Account{} = account, attrs) do
+    account
+    |> Account.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a account.
+
+  ## Examples
+
+      iex> delete_account(account)
+      {:ok, %Account{}}
+
+      iex> delete_account(account)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_account(%Account{} = account) do
+    Repo.delete(account)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking account changes.
+
+  ## Examples
+
+      iex> change_account(account)
+      %Ecto.Changeset{data: %Account{}}
+
+  """
+  def change_account(%Account{} = account, attrs \\ %{}) do
+    Account.changeset(account, attrs)
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,12 +1,27 @@
 defmodule BankingApi.Factory do
-    use ExMachina.Ecto, repo: BankingApi.Repo
-  
-    alias BankingApi.Accounts.User
+  @moduledoc false
 
-    def user_factory do
-      %User{
-        email: "user@email.com",
-        password: "123123"
-      }
-    end
+  use ExMachina.Ecto, repo: BankingApi.Repo
+
+  alias BankingApi.Accounts.User
+  alias BankingApi.Bank.Account
+
+  def user_factory do
+    email = sequence(:email, &"user#{&1}@email.com")
+
+    %User{
+      email: email,
+      password: "123123"
+    }
+  end
+
+  def account_factory do
+    user = insert(:user)
+
+    %Account{
+      name: "Cash",
+      type: "asset",
+      user_id: user.id
+    }
+  end
 end
